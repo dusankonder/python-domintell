eba85058a371bcb2a745cd973105babacefb1a08
Taserface <130240685+dusankonder@users.noreply.github.com>
2024-04-06 23:50:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open('README.md') as file:
 
 setup(name='python-domintell',
       version='0.0.18',
-      url='https://github.com/shamanenas/python-domintell',
+      url='https://github.com/dusan.konder/python-domintell',
       license='MIT',
       author='Zilvinas Binisevicius',
       install_requires=["pyserial"],
-      author_email='zilvinas@binis.me',
+      author_email='-',
       description="Python Library for the Domintell protocol",
       long_description=long_description,
       long_description_content_type="text/markdown",
